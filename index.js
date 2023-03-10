6d1f7fe2405188638f05d2cf64d7556853fc4225
Pedro Henrique <119066915+Pedrocoro0601@users.noreply.github.com>
2023-03-10 16:40:10 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+var formacao = document.getElementById("formacao")
+var iron = document.getElementById("iron")
+var panteranegra = document.getElementById("panteranegra")
+
+document.addEventListener('scroll', () => {   
+        iron.classList.add('homemdeferro')  
+        setInterval(()=>{
+            panteranegra.classList.add('panteranegra')  
+        }, 2000)
+        
+})
