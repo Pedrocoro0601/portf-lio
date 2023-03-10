babd0214786aead39eda6791de6a9d1bc8bdcddd
Pedro Henrique <119066915+Pedrocoro0601@users.noreply.github.com>
2023-03-10 20:33:34 -0300

    Add files via upload

---
+++ portf-lio-main/index.js
@@ -0,0 +1,13 @@
+/*
+var formacao = document.getElementById("formacao")
+var iron = document.getElementById("iron")
+var panteranegra = document.getElementById("panteranegra")
+
+document.addEventListener('scroll', () => {   
+        iron.classList.add('homemdeferro')  
+        setInterval(()=>{
+            panteranegra.classList.add('panteranegra')  
+        }, 2000)
+        
+})
+*/
